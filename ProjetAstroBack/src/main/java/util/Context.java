4a5ac22e9edcc2be545bd4e611f4fd7b686fa41b
Yannis <yannis.benrida@gadz.org>
2021-08-18 16:38:58 +0200

    Modif context

--- ProjetAstroBack/src/main/java/util/Context.java
+++ ProjetAstroBack/src/main/java/util/Context.java
@@ -1,5 +1,29 @@
 package util;
 
-public class Context {
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import DAO.IDAOCompte;
+import DAO.IDAOPositions;
+import DAO.IDAOSysteme;
+import DAO.IDAOSystemeInit;
+
+
+import DAO.jpa.DAOCompteJPA;
+import DAO.jpa.*;
+import metier.Compte;
+
+public class Context {
+	private Compte connected=null;
+	private IDAOCompte daoC = new DAOComptejpa();
+	private IDAOPositions daoP = new DAOPositionsjpa();
+	private IDAOSysteme daoS = new DAOSsystemejpa();
+	private IDAOSystemeInit daoSI = new DAOSsystemeInitjpa();
+
+	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("astro-persistence");
+	
+	private static Context _instance=null;
+	
+	
+	
 }
