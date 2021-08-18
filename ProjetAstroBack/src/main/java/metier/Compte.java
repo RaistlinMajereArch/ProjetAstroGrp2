b81426976937f01fb8b8ce34fdc41962d087e001
SuzanneVZ <vanzuijlen.suzanne@gmail.com>
2021-08-18 17:19:06 +0200

    change Compte.jave

--- ProjetAstroBack/src/main/java/metier/Compte.java
+++ ProjetAstroBack/src/main/java/metier/Compte.java
@@ -1,6 +1,19 @@
 package metier;
 
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="type_compte",columnDefinition = "ENUM('admin', 'user')")
 public abstract class Compte {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected int id;
 	protected String login;
 	protected String password;
