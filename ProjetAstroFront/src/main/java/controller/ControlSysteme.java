621da758424f69ecca7011651bc6a3cdee2e3771
Raistlin <victor.douet38@gmail.com>
2021-08-20 10:18:57 +0200

    ControlSystemInit

--- ProjetAstroFront/src/main/java/controller/ControlSysteme.java
+++ ProjetAstroFront/src/main/java/controller/ControlSysteme.java
@@ -3,6 +3,7 @@ package controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 
 
-public class ControlSysteme {
+public class ControlSysteme extends HttpServlet{
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int timestep=Integer.parseInt(request.getParameter("timestep"));
