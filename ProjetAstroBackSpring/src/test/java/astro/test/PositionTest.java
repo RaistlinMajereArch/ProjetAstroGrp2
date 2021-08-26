26a1b968d919d25419e74d38b78378f3280a048c
Yannis <yannis.benrida@gadz.org>
2021-08-26 10:30:32 +0200

    classe test

---
+++ ProjetAstroBackSpring/src/test/java/astro/test/PositionTest.java
@@ -0,0 +1,14 @@
+package astro.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PositionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ ProjetAstroBackSpring/src/test/java/astro/test/SystemeInitTest.java
@@ -0,0 +1,14 @@
+package astro.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SystemeInitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
