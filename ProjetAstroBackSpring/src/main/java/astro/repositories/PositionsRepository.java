1549146c0c6a7a8df0cf53e05d6771caf17684f9
SuzanneVZ <vanzuijlen.suzanne@gmail.com>
2021-08-26 10:24:26 +0200

    positionRepository

--- ProjetAstroBackSpring/src/main/java/astro/repositories/PositionsRepository.java
+++ ProjetAstroBackSpring/src/main/java/astro/repositories/PositionsRepository.java
@@ -1,5 +1,17 @@
 package astro.repositories;
 
-public interface PositionsRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import astro.metier.Position;
+
+
+
+public interface PositionsRepository extends JpaRepository<Position, Integer> {
+	
+	List<Position> findByid_corpsCeleste(Integer id);
+	
+	List<Position> findByid_timeStep(Integer t);
 
 }
