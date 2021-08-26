b5cef905b9b945427f5778f62d4acc0db49e217b
SuzanneVZ <vanzuijlen.suzanne@gmail.com>
2021-08-26 11:42:24 +0200

    PositionTest

--- ProjetAstroBackSpring/src/test/java/astro/test/PositionTest.java
+++ ProjetAstroBackSpring/src/test/java/astro/test/PositionTest.java
@@ -3,12 +3,57 @@ package astro.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import astro.metier.Position;
+import astro.repositories.PositionsRepository;
+import astro.config.AppConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { AppConfig.class })
 public class PositionTest {
+	private static AnnotationConfigApplicationContext ctx;
+	private PositionsRepository posRepo;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Rollback(true)
+	@Transactional
+	public void testInsert() {
+		Position posIns = new Position(0,1,0.0,0.0);
+		posIns = posRepo.save(posIns);
+		assertEquals(posIns,posRepo.findById(posIns.getId()).get());
+	}
+	
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void testDelete() {
+		Position posIns = new Position(0,1,0.0,0.0);
+		posIns = posRepo.save(posIns);
+		posRepo.delete(posIns);
+		assertFalse(posRepo.findById(posIns.getId()).isPresent());
 	}
 
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void testFindByIdCorps() {
+		Position p = new Position(0,1,0.0,0.0);
+		posRepo.save(p);
+		assertNotNull(posRepo.findByIdCorpsCeleste(p.getIdCorpsCeleste()));
+	}
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void testFindByIdTimeStep() {
+		Position p = new Position(0,1,0.0,0.0);
+		posRepo.save(p);
+		assertNotNull(posRepo.findByIdTimeStep(p.getIdTimeStep()));
+	}
 }
+
