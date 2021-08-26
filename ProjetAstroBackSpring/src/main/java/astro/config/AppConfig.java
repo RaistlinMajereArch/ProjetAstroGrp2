c0f560dc71b92dab1a648638ad88a0fa55cabc14
Yannis <yannis.benrida@gadz.org>
2021-08-26 10:04:56 +0200

    mise en route

--- ProjetAstroBackSpring/src/main/java/astro/config/AppConfig.java
+++ ProjetAstroBackSpring/src/main/java/astro/config/AppConfig.java
@@ -5,10 +5,9 @@ import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.hibernate.cfg.Environment;
+import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
