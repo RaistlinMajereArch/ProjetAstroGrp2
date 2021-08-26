e4d6a447eac29e8c405ef6767a92d611bae57b66
Yannis <yannis.benrida@gadz.org>
2021-08-26 14:58:27 +0200

    systemeInitService

--- ProjetAstroBackSpring/src/main/java/astro/repositories/SystemeInitRepository.java
+++ ProjetAstroBackSpring/src/main/java/astro/repositories/SystemeInitRepository.java
@@ -14,9 +14,9 @@ public interface SystemeInitRepository extends JpaRepository<CorpsCeleste, Integ
 	@Query("select distinct c from CorpsCeleste c where c.nom like :lib or c.id like :lib or c.type like :lib")
 	public List<CorpsCeleste> filterCorps(@Param("lib") String texte);
 	
-	@Query("UPDATE systeminit SET parent_id= NULL WHERE systeminit.id =:id")
-	public void updateParent(@Param("id") Integer id);
+	@Query("update CorpsCeleste c set c.parent=NULL WHERE c.id =:id")
+	public void updateIdParent(@Param("id") Integer id);
 	
-	@Query(" DELETE FROM systeminit WHERE systeminit.id_parent =:id")
-	public void deleteEnfants(@Param("id") Integer id);
+	@Query("select distinct c from CorpsCeleste c where c.idParent=:id")
+	public List<CorpsCeleste> selectEnfants(@Param("id") Integer id);
 }
