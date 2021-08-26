5c1311b454cc9110269aa15ad3bb62f7481d052f
Raistlin <victor.douet38@gmail.com>
2021-08-26 10:31:24 +0200

    Correction Connexion

--- ProjetAstroBackSpring/src/main/java/astro/repositories/CompteRepository.java
+++ ProjetAstroBackSpring/src/main/java/astro/repositories/CompteRepository.java
@@ -1,12 +1,15 @@
 package astro.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import astro.metier.Compte;
+
 public interface CompteRepository {
 
-	@Query("from Compte c where c.login = :login and c.password=:password")
-	public void seConnecter(@Param("login")String login, @Param("password")String password) ;
+	Optional<Compte> findByLoginAndPassword(String login, String password);
 	
 	@Query("from Compte c where c.login like :lib or c.password like :lib")
 	public void filterCompte(@Param("lib")String lib) ;
