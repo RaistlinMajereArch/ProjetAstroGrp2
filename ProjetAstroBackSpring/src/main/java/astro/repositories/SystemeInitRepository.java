b28dbc987b212754ce16e66471fdbefef56b2f38
Yannis <yannis.benrida@gadz.org>
2021-08-26 16:01:04 +0200

    Systeminit qui marche pas

--- ProjetAstroBackSpring/src/main/java/astro/repositories/SystemeInitRepository.java
+++ ProjetAstroBackSpring/src/main/java/astro/repositories/SystemeInitRepository.java
@@ -3,8 +3,10 @@ package astro.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import astro.metier.CorpsCeleste;
 
@@ -14,9 +16,11 @@ public interface SystemeInitRepository extends JpaRepository<CorpsCeleste, Integ
 	@Query("select distinct c from CorpsCeleste c where c.nom like :lib or c.id like :lib or c.type like :lib")
 	public List<CorpsCeleste> filterCorps(@Param("lib") String texte);
 	
-	@Query("update CorpsCeleste c set c.parent=NULL WHERE c.id =:id")
+	@Transactional
+	@Modifying
+	@Query("update CorpsCeleste c set c.parent=null WHERE c.id =:id")
 	public void updateIdParent(@Param("id") Integer id);
 	
-	@Query("select distinct c from CorpsCeleste c where c.Parent=:parent")
+	@Query("select distinct c from CorpsCeleste c where c.parent=:parent")
 	public List<CorpsCeleste> selectEnfants(@Param("parent") CorpsCeleste c);
 }
