728d5b9cb1604824039e96f01f560f21cdf1f722
Yannis <yannis.benrida@gadz.org>
2021-09-03 14:55:26 +0200

    Throw appTest

--- ProjetAstroBackSpring/src/test/java/test/AppTest.java
+++ ProjetAstroBackSpring/src/test/java/test/AppTest.java
@@ -1,12 +1,15 @@
 package test;
 
+import java.io.IOException;
+
+import org.springframework.beans.BeansException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import astro.AppSpring;
 import astro.config.AppConfig;
 
 public class AppTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws BeansException, IOException {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
 		// spring
 		ctx.getBeanFactory().createBean(AppSpring.class).run(args);
