93e5e679b5ed950f4af39057b62b9677621ea671
Yannis <yannis.benrida@gadz.org>
2021-09-09 11:22:03 +0200

    CCRC

--- ProjetAstroSpringBoot/src/main/java/SoprAjc/ProjetAstroSpringBoot/restController/CorpsCelesteRestController.java
+++ ProjetAstroSpringBoot/src/main/java/SoprAjc/ProjetAstroSpringBoot/restController/CorpsCelesteRestController.java
@@ -8,8 +8,8 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.ReflectionUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -28,6 +29,7 @@ import SoprAjc.ProjetAstroSpringBoot.exceptions.CorpsCelesteException;
 import SoprAjc.ProjetAstroSpringBoot.model.CorpsCeleste;
 import SoprAjc.ProjetAstroSpringBoot.model.JsonViews;
 import SoprAjc.ProjetAstroSpringBoot.repositories.SystemeInitRepository;
+
 
 @RestController
 @RequestMapping("/api/CorpsCeleste")
@@ -89,12 +91,22 @@ public class CorpsCelesteRestController {
 		Optional<CorpsCeleste> opt = sysIRepo.findById(id);
 		if (opt.isPresent()) {
 			CorpsCeleste corpsCelesteEnBase = opt.get();
-			fields.forEach((key, value) -> {
+			fields.forEach((key,value)->{
 				Field field = ReflectionUtils.findField(CorpsCeleste.class, key);
 				ReflectionUtils.makeAccessible(field);
 				ReflectionUtils.setField(field, corpsCelesteEnBase, value);
-			)};
+			});
 			return sysIRepo.save(corpsCelesteEnBase);
+		}
+		throw new CorpsCelesteException();
+	}
+	
+	@PutMapping("/{id}")
+	@JsonView(JsonViews.Common.class)
+	public CorpsCeleste replace(@Valid @RequestBody CorpsCeleste corpsCeleste, BindingResult br, @PathVariable Integer id) {
+		if (br.hasErrors()) {
+			corpsCeleste.setId(id);
+			return sysIRepo.save(corpsCeleste);
 		}
 		throw new CorpsCelesteException();
 	}
