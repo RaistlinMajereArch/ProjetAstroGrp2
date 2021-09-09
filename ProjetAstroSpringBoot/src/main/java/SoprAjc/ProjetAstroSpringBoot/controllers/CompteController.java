eaffb37c0c5ad3d8961159df7e26b514f0a319e4
Yannis <yannis.benrida@gadz.org>
2021-09-09 12:04:45 +0200

    CompteController

--- ProjetAstroSpringBoot/src/main/java/SoprAjc/ProjetAstroSpringBoot/controllers/CompteController.java
+++ ProjetAstroSpringBoot/src/main/java/SoprAjc/ProjetAstroSpringBoot/controllers/CompteController.java
@@ -1,5 +1,13 @@
 package SoprAjc.ProjetAstroSpringBoot.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CompteController {
 
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
 }
